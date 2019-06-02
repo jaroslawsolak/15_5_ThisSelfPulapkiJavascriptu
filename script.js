d97fb4ff3c0ca262f087c444ee670ad46158839e
jaroslawsolak <47710528+jaroslawsolak@users.noreply.github.com>
2019-06-02 11:49:03 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-function Phone(brand, price, color, avalibility) {
-	this.brand = brand;
-	//by using this, the "brand" property of the object we create will assume the value of the brand argument
-	this.price = price;
-	this.color = color;
-	this.avalibility = avalibility;
+function Button(text) {
+	this.text = text || 'Hello';
 }
 
-Phone.prototype.printInfo = function() {
-	console.log("The phone brand is " + this.brand + ", color is " + this.color + ", the price is " + this.price + " and the avalibility is " + this.price + ".");
+Button.prototype = {
+	create: function () {
+		var self = this;
+		this.element = document.createElement('button');
+		this.element.innerText = this.text;
+		this.element.addEventListener('click', function () {
+			alert(self.text);
+		});
+		document.body.appendChild(this.element);
+	}
 }
 
-var iPhone6S = new Phone("Apple", 2250, "silver", "available");
-var SamsungGalaxyS6 = new Phone("Samsung", 1500, "gold", "not available");
-var OnePlusOne = new Phone("Apple", 2000, "gray", "available");
+var btn1 = new Button('Hello!');
 
-iPhone6S.printInfo();
-SamsungGalaxyS6.printInfo();
-OnePlusOne.printInfo();
-
-
-
+btn1.create();
